97daa95588b43727c5be568c4e491aaee35630da
iwill4u <iwill4u2@naver.com>
2022-11-13 12:50:26 +0900

    코드펜 로그인 화면 수정

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,14 +1,62 @@
 "use strict";
 
-const home = (req, res) => {
-    res.render("home/index");
+// const UserStorage = require("../../models/UserStorage");
+const User = require("../../models/User");
+
+const output = {
+    home: (req, res) => {
+        res.render("home/index");
+    },    
+    login: (req, res) => {
+        res.render("home/login")
+    },
+}
+
+
+
+const process = {
+    login: (req, res) => {
+        const user = new User(req.body);
+        const response = user.login();
+        console.log(response);
+        return res.json(response);
+
+        /* ------------------------------------------------------------
+        const id = req.body.id,
+          pw = req.body.pw;
+
+        // const userStorage = new UserStorage();
+        // console.log(userStorage.users);
+
+        // console.log(UserStorage.getUsers("id","pw"));
+        const users = UserStorage.getUsers("id","pw");
+
+        const response = {};  // 선언 --> {} : Object, [] : Array
+
+        if (users.id.includes(id)){
+            const idx = users.id.indexOf(id);
+            if (users.pw[idx] === pw) {
+                // 1.response 를 이용
+                response.success = true;
+                return res.json(response);
+
+                // 2.아래의 Logic를 1.response를 이용하는 것으로 수정
+                // return res.json({
+                //     success: true,
+                // });
+            }
+        }
+
+        response.success = false;
+        response.msg = "로그인에 실패하였습니다.";
+
+        return res.json(response);
+        ------------------------------------------------------------ */
+    },
 };
 
-const login = (req, res) => {
-    res.render("home/login")
-};
 
 module.exports = {
-    home,
-    login,
+    output,
+    process,
 }
